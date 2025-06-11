184f836a63ce88bb7fa5885b85701d16ff53d336
Kavin <140266232+Kavin302004@users.noreply.github.com>
2025-06-11 17:19:22 +0530

    Delete src directory

--- src/main/java/com/cab/book/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.cab.book.entity;
-
-public enum Role {
-	ROLE_CUSTOMER,
-	ROLE_DRIVER,
-	ROLE_ADMIN
-}
--- src/main/java/com/cab/book/repository/DriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cab.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cab.book.entity.Driver;
-
-public interface DriverRepository extends JpaRepository<Driver, Long> {
-	
-}
